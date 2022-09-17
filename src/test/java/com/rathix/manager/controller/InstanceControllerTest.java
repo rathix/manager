e3925c58d90b793832d71296c2d6fd8b37ece8cd
Kenny Andries <rathix@protonmail.ch>
2022-09-17 15:49:51 +0200

    Implement unit & integration tests

---
+++ src/test/java/com/rathix/manager/controller/InstanceControllerTest.java
@@ -0,0 +1,157 @@
+package com.rathix.manager.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.rathix.manager.exception.AlreadyExistsException;
+import com.rathix.manager.model.Instance;
+import com.rathix.manager.service.InstanceService;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.NoSuchElementException;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+/**
+ * Integration tests for the controller
+ */
+@ExtendWith(SpringExtension.class)
+@WebMvcTest(InstanceController.class)
+class InstanceControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+    @Autowired
+    private ObjectMapper objectMapper;
+    @MockBean
+    private InstanceService instanceService;
+
+    private Instance instance;
+    private List<Instance> instanceList;
+
+    @BeforeEach
+    void setUp() {
+        instance = new Instance();
+        instance.setName("testInstance");
+        instance.setUrl("testUrl");
+        instance.setId(0L);
+
+        instanceList = new ArrayList<>();
+        instanceList.add(instance);
+    }
+
+    @AfterEach
+    void tearDown() {
+        instance = null;
+        instanceList = null;
+    }
+
+    /**
+     * Creating an instance should return 201
+     */
+    @Test
+    void createInstance() throws Exception {
+        when(instanceService.createInstance(any(Instance.class))).thenReturn(instance);
+
+        mockMvc.perform(post("/api/v1/instance")
+                        .contentType("application/json")
+                        .content(objectMapper.writeValueAsString(instance)))
+                .andExpect(status().isCreated());
+    }
+
+    /**
+     * Creating a duplicate instance should return 400
+     */
+    @Test
+    void createDuplicateInstance() throws Exception {
+        when(instanceService.createInstance(any(Instance.class))).thenThrow(new AlreadyExistsException());
+
+        mockMvc.perform(post("/api/v1/instance")
+                        .contentType("application/json")
+                        .content(objectMapper.writeValueAsString(instance)))
+                .andExpect(status().isBadRequest());
+
+    }
+
+    /**
+     * Retrieving all instances should return 302
+     */
+    @Test
+    void retrieveInstances() throws Exception {
+        when(instanceService.retrieveInstances()).thenReturn(instanceList);
+
+        mockMvc.perform(get("/api/v1/instance"))
+                .andExpect(status().isFound());
+    }
+
+    /**
+     * Retrieving an instance should return 302
+     */
+    @Test
+    void retrieveInstance() throws Exception {
+        when(instanceService.retrieveInstance(any(Long.class))).thenReturn(instance);
+
+        mockMvc.perform(get("/api/v1/instance/{id}", instance.getId()))
+                .andExpect(status().isFound());
+    }
+
+    /**
+     * Retrieving an instance which doesn't exist should return 404
+     */
+    @Test
+    @Disabled
+    void retrieveInstanceNotFound() throws Exception {
+        when(instanceService.retrieveInstance(any(Long.class))).thenThrow(new NoSuchElementException());
+
+        mockMvc.perform(get("/api/v1/instance/{id}", instance.getId()))
+                .andExpect(status().isNotFound());
+    }
+
+    /**
+     * Updating an instance should return 200
+     */
+    @Test
+    void updateInstance() throws Exception {
+        when(instanceService.updateInstance(any(Long.class), any(Instance.class))).thenReturn(instance);
+
+        mockMvc.perform(put("/api/v1/instance/{id}", instance.getId())
+                        .contentType("application/json")
+                        .content(objectMapper.writeValueAsString(instance)))
+                .andExpect(status().isOk());
+    }
+
+    /**
+     * Updating an instance should return 304
+     */
+    @Test
+    void updateDuplicateInstance() throws Exception {
+        when(instanceService.updateInstance(any(Long.class), any(Instance.class))).thenThrow(new AlreadyExistsException());
+
+        mockMvc.perform(put("/api/v1/instance/{id}", instance.getId())
+                        .contentType("application/json")
+                        .content(objectMapper.writeValueAsString(instance)))
+                .andExpect(status().isNotModified());
+    }
+
+    /**
+     * Deleting an instance should return 410
+     */
+    @Test
+    void deleteInstance() throws Exception {
+        when(instanceService.deleteInstance(any(Long.class))).thenReturn(true);
+
+        mockMvc.perform(delete("/api/v1/instance/{id}", instance.getId()))
+                .andExpect(status().isGone());
+    }
+}
---
+++ src/test/java/com/rathix/manager/service/InstanceServiceTest.java
@@ -0,0 +1,125 @@
+package com.rathix.manager.service;
+
+import com.rathix.manager.exception.AlreadyExistsException;
+import com.rathix.manager.model.Instance;
+import com.rathix.manager.repository.InstanceRepository;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+
+/**
+ * Unit tests for the InstanceService class
+ */
+@ExtendWith(MockitoExtension.class)
+class InstanceServiceTest {
+    @Mock
+    private InstanceRepository instanceRepository;
+    @Autowired
+    @InjectMocks
+    private InstanceService instanceService;
+    private Instance instance;
+    private List<Instance> instanceList;
+
+    @BeforeEach
+    void setUp() {
+        instance = new Instance();
+        instance.setName("testInstance");
+        instance.setUrl("testUrl");
+        instance.setId(0L);
+
+        instanceList = new ArrayList<>();
+        instanceList.add(instance);
+    }
+
+    @AfterEach
+    void tearDown() {
+        instance = null;
+        instanceList = null;
+    }
+
+    /**
+     * Saving an instance should work
+     */
+    @Test
+    void createInstance() {
+        when(instanceRepository.save(any(Instance.class))).thenReturn(instance);
+        Instance savedInstance = instanceRepository.save(instance);
+        assertEquals(savedInstance.getName(), "testInstance");
+    }
+
+    /**
+     * Saving a duplicate instance should throw an AlreadyExistsException
+     */
+    @Test
+    void createDuplicateInstance() {
+        when(instanceRepository.existsInstanceByName(any(String.class))).thenReturn(true);
+        assertThrows(AlreadyExistsException.class, () -> instanceService.createInstance(instance));
+    }
+
+    /**
+     * Retrieving all instances should work
+     */
+    @Test
+    void retrieveInstances() {
+        when(instanceRepository.findAll()).thenReturn(instanceList);
+        List<Instance> returnList = instanceService.retrieveInstances();
+        assertEquals(returnList.get(0).getName(), "testInstance");
+    }
+
+    /**
+     * Retrieving an instance which doesn't exist should throw a NoSuchElementException
+     */
+    @Test
+    void retrieveInstance() {
+        when(instanceRepository.findById(any(Long.class))).thenReturn(Optional.empty());
+        assertThrows(NoSuchElementException.class, () -> instanceService.retrieveInstance(0L));
+    }
+
+    /**
+     * Updating an instance should work
+     */
+    @Test
+    void updateInstance() throws AlreadyExistsException {
+        when(instanceRepository.findById(any(Long.class))).thenReturn(Optional.of(instance));
+
+        Instance newInstance = new Instance();
+        newInstance.setUrl("newTestUrl");
+        newInstance.setName("newTestName");
+
+        when(instanceRepository.save(any(Instance.class))).thenReturn(newInstance);
+        Instance savedNewInstance = instanceService.updateInstance(0L, newInstance);
+
+        assertEquals(savedNewInstance.getName(), "newTestName");
+    }
+
+    /**
+     * Updating an instance with a name or url which already exists should throw an AlreadyExistsException
+     */
+    @Test
+    void updateDuplicateInstance() {
+        when(instanceRepository.existsInstanceByName(any(String.class))).thenReturn(true);
+        assertThrows(AlreadyExistsException.class, () -> instanceService.updateInstance(0L, instance));
+    }
+
+    /**
+     * Deleting an instance should work
+     */
+    @Test
+    void deleteInstance() {
+        assertTrue(instanceService.deleteInstance(0L));
+    }
+}
